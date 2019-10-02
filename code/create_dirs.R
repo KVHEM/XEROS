e397749672eccfe57a0403e844d8fd5338ccaa97
Yannis Markonis <imarkonis@gmail.com>
2019-10-02 12:40:59 +0200

    installs packages

--- code/create_dirs.R
+++ code/create_dirs.R
@@ -1,5 +1,9 @@
-# Creates directory structure for first time
-source('./code/main.R')
+# Creates directory structure for first time and installs all the packages needed for the project
+proj_packages <- c("tidyverse", "data.table")
+
+if (length(setdiff(proj_packages, rownames(installed.packages()))) > 0) {
+  install.packages(setdiff(proj_packages, rownames(installed.packages())))  
+}
 
 dir.create('../data')
 dir.create('../data/input')
